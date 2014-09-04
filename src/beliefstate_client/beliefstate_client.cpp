9f4b9ede5a52e2e9354a502f2508cfc1762ee6ce
Jan Winkler <winkler@cs.uni-bremen.de>
2014-09-04 16:56:07 +0200

    Cleaned up, added convenience constructor, documentation, made code nicer

--- src/beliefstate_client/beliefstate_client.cpp
+++ src/beliefstate_client/beliefstate_client.cpp
@@ -1,11 +1,61 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014, Jan Winkler, Institute for Artificial
+ *  Intelligence, Universität Bremen.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Institute for Artificial Intelligence,
+ *     Universität Bremen, nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/** \author Jan Winkler */
+
+
 #include <beliefstate_client/beliefstate_client.h>
 
 
-BeliefstateClient::BeliefstateClient(int argc, char** argv, string strSource) {
+BeliefstateClient::BeliefstateClient(std::string strSource) {
+  this->init(0, NULL, strSource);
+}
+
+BeliefstateClient::BeliefstateClient(int argc, char** argv, std::string strSource) {
+  this->init(argc, argv, strSource);
+}
+
+BeliefstateClient::~BeliefstateClient() {
+}
+
+void BeliefstateClient::init(int argc, char** argv, std::string strSource) {
   m_strServer = "/beliefstate_ros";
   this->setSource(strSource);
   
-  if(!ros::ok()) {
+  if(!ros::ok() && argc > 0 && argv) {
     ros::init(argc, argv, strSource);
     m_nhHandle = new ros::NodeHandle("~");
   } else {
@@ -18,36 +68,29 @@ BeliefstateClient::BeliefstateClient(int argc, char** argv, string strSource) {
   m_sclAlterContextService = m_nhHandle->serviceClient<designator_integration_msgs::DesignatorCommunication>(m_strServer + "/alter_context");
 }
 
-BeliefstateClient::~BeliefstateClient() {
-}
-
-list<CDesignator*> BeliefstateClient::callService(ros::ServiceClient sclServiceClient, CDesignator* desigContent) {
+std::list<CDesignator*> BeliefstateClient::callService(ros::ServiceClient sclServiceClient, CDesignator* desigContent) {
   designator_integration_msgs::DesignatorCommunication dcComm;
   dcComm.request.request.designator = desigContent->serializeToMessage();
   
-  list<CDesignator*> lstReturnDesigs;
+  std::list<CDesignator*> lstReturnDesigs;
   if(sclServiceClient.call(dcComm)) {
-    vector<designator_integration_msgs::Designator> vecDesigResponses = dcComm.response.response.designators;
-    
-    for(vector<designator_integration_msgs::Designator>::iterator itDesig = vecDesigResponses.begin();
-	itDesig != vecDesigResponses.end();
-	itDesig++) {
-      lstReturnDesigs.push_back(new CDesignator((*itDesig)));
+    for(designator_integration_msgs::Designator msgDesig : dcComm.response.response.designators) {
+      lstReturnDesigs.push_back(new CDesignator(msgDesig));
     }
   }
   
   return lstReturnDesigs;
 }
 
-void BeliefstateClient::setSource(string strSource) {
+void BeliefstateClient::setSource(std::string strSource) {
   m_strSource = strSource;
 }
 
-string BeliefstateClient::source() {
+std::string BeliefstateClient::source() {
   return m_strSource;
 }
 
-int BeliefstateClient::startContext(string strContextName, int nTimeStamp) {
+int BeliefstateClient::startContext(std::string strContextName, int nTimeStamp) {
   CDesignator* desigRequest = new CDesignator();
   desigRequest->setType(ACTION);
   desigRequest->setValue(string("_name"), strContextName);
@@ -58,8 +101,7 @@ int BeliefstateClient::startContext(string strContextName, int nTimeStamp) {
     desigRequest->setValue("time-start", nTimeStamp);
   }
   
-  list<CDesignator*> lstDesigs = this->callService(m_sclBeginContextService,
-						   desigRequest);
+  std::list<CDesignator*> lstDesigs = this->callService(m_sclBeginContextService, desigRequest);
   delete desigRequest;
   
   int nID = -1;
@@ -68,10 +110,8 @@ int BeliefstateClient::startContext(string strContextName, int nTimeStamp) {
     nID = (int)desigResponse->floatValue("_id");
   }
   
-  for(list<CDesignator*>::iterator itDesig = lstDesigs.begin();
-      itDesig != lstDesigs.end();
-      itDesig++) {
-    delete *itDesig;
+  for(CDesignator* cdDesig : lstDesigs) {
+    delete cdDesig;
   }
   
   return nID;
@@ -89,39 +129,36 @@ void BeliefstateClient::endContext(int nContextID, bool bSuccess, int nTimeStamp
     desigRequest->setValue("time-end", nTimeStamp);
   }
   desigRequest->printDesignator();
-  list<CDesignator*> lstDesigs = this->callService(m_sclEndContextService,
-						   desigRequest);
+  list<CDesignator*> lstDesigs = this->callService(m_sclEndContextService, desigRequest);
   
-  for(list<CDesignator*>::iterator itDesig = lstDesigs.begin();
-      itDesig != lstDesigs.end();
-      itDesig++) {
-    delete *itDesig;
+  for(CDesignator* cdDesig : lstDesigs) {
+    delete cdDesig;
   }
-
+  
   delete desigRequest;
 }
 
-list<CDesignator*> BeliefstateClient::alterContext(CDesignator* desigAlter) {
+std::list<CDesignator*> BeliefstateClient::alterContext(CDesignator* desigAlter) {
   desigAlter->setValue(string("_source"), m_strSource);
   
   return this->callService(m_sclAlterContextService, desigAlter);
 }
 
-void BeliefstateClient::exportFiles(string strFilename) {
+void BeliefstateClient::exportFiles(std::string strFilename) {
   CDesignator* desigRequest = new CDesignator();
   desigRequest->setType(ACTION);
   
-  desigRequest->setValue(string("command"), "export-planlog");
-  desigRequest->setValue(string("format"), "owl");
-  desigRequest->setValue(string("filename"), strFilename + ".owl");
-  desigRequest->setValue(string("show-successes"), 1);
-  desigRequest->setValue(string("show-fails"), 1);
-  desigRequest->setValue(string("max-detail-level"), 99);
+  desigRequest->setValue(std::string("command"), "export-planlog");
+  desigRequest->setValue(std::string("format"), "owl");
+  desigRequest->setValue(std::string("filename"), strFilename + ".owl");
+  desigRequest->setValue(std::string("show-successes"), 1);
+  desigRequest->setValue(std::string("show-fails"), 1);
+  desigRequest->setValue(std::string("max-detail-level"), 99);
   
   this->callService(m_sclAlterContextService, desigRequest);
   
-  desigRequest->setValue(string("format"), "dot");
-  desigRequest->setValue(string("filename"), strFilename + ".dot");
+  desigRequest->setValue(std::string("format"), "dot");
+  desigRequest->setValue(std::string("filename"), strFilename + ".dot");
   
   this->callService(m_sclAlterContextService, desigRequest);
   
